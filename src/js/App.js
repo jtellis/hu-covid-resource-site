32c64c44b44ce5acf884a58a8b70807d4d9830bb
jtellis <jessetellis@gmail.com>
2020-10-04 05:10:21 -0400

    Create custom Tableau component

--- src/js/App.js
+++ src/js/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import TableauReport from 'tableau-react';
+import TableauViz from './TableauViz';
 
 render (
     <React.StrictMode>
         <header>NYS COVID-19 Parent Portal</header>
         <nav></nav>
         <main>
-            <TableauReport url='https://public.tableau.com/views/NYSCovidDashboard/Sheet1' />
+            <TableauViz url='https://public.tableau.com/views/NYSCovidDashboard/Sheet1' />
         </main>
         <footer>
             <i>NYS COVID-19 Parent Portal</i> was created during Hack Upstate XV.
---
+++ src/js/TableauViz/index.js
@@ -0,0 +1,35 @@
+/*
+Referenced: https://github.com/coopermaruyama/tableau-react
+    Imported Tableau was used from this project
+*/
+
+import React, { useRef, useEffect } from 'react';
+import { render } from 'react-dom';
+
+import tableau from './tableau-2.3.0';
+
+function TableauViz ({ url, options={} }) {
+
+    let containerRef = useRef(null);
+
+    useEffect(() => {
+        let viz = new tableau.Viz(containerRef.current, url, options);
+        viz.addEventListener(tableau.TableauEventName.MARKS_SELECTION, onMarksSelection);
+    }, []);
+
+    return (
+        <div>
+            <div ref={containerRef} />
+        </div>
+    );
+}
+
+function onMarksSelection(marksEvent) {
+    return marksEvent.getMarksAsync().then(reportSelectedMarks);
+}
+
+function reportSelectedMarks(marks) {
+    console.log(marks);
+}
+
+export default TableauViz;
