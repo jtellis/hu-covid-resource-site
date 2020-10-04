0212821af4eac6bffdf0c635c409ffe40a63e76c
jtellis <jessetellis@gmail.com>
2020-10-04 04:25:20 -0400

    Give project a proper name

--- src/js/App.js
+++ src/js/App.js
@@ -5,12 +5,14 @@ import TableauReport from 'tableau-react';
 
 render (
     <React.StrictMode>
-        <header>Hack Upstate - COVID-19 Impact & Resources</header>
+        <header>NYS COVID-19 Parent Portal</header>
         <nav></nav>
         <main>
             <TableauReport url='https://public.tableau.com/views/NYSCovidDashboard/Sheet1' />
         </main>
-        <footer>Created for Hack Upstate XV</footer>
+        <footer>
+            <i>NYS COVID-19 Parent Portal</i> was created during Hack Upstate XV.
+        </footer>
     </React.StrictMode>,
 
     document.getElementById('app')
