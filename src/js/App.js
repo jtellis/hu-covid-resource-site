078a7af2bc29e2641ea98047909f055956516846
jtellis <jessetellis@gmail.com>
2020-10-04 00:14:30 -0400

    Add initial Tableau viz embed

--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import TableauReport from 'tableau-react';
+
 render (
     <React.StrictMode>
+        <TableauReport url='https://public.tableau.com/views/NYSCovidDashboard/Sheet1' />
     </React.StrictMode>,
 
     document.getElementById('app')
